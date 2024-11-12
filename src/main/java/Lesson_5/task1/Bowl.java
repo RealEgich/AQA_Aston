a51d624cc88ec214b3a9608dd8a8be8e6d9295cb
Egor Kravchenko <61183935+RealEgich@users.noreply.github.com>
2024-11-12 18:19:33 +0300

    Add files via upload

---
+++ src/main/java/Lesson_5/task1/Bowl.java
@@ -0,0 +1,14 @@
+package Lesson_5.task1;
+
+public class Bowl {
+    private int volume;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+}
