af24e4985f332c8cc2c5d59a0d38a19f02854065
Egor Kravchenko <61183935+RealEgich@users.noreply.github.com>
2024-11-14 12:15:48 +0300

    Add files via upload

---
+++ src/main/java/Lesson_6/MyArrayDataException.java
@@ -0,0 +1,15 @@
+package Lesson_6;
+
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException() {
+        super("Ошибка формата данных.");
+    }
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String element, int i, int j) {
+        this(String.format("Неверный формат для преобразования в: %s, на позиции [%s][%s]", element, i, j));
+    }
+}
